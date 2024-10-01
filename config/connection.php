fc46b354aa4f1dd62d45be2d548128ff78c9d95e
little-python-guy <matat201@gmail.com>
2024-10-01 20:56:57 -0300

    creating database connection file

---
+++ config/connection.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $host = 'localhost';
+    $username = 'admin';
+    $password = '';
+    $bd_name = 'game_journey_bd';
+    
+    $connect = mysqli_connect($host, $username, $password, $bd_name);
+
+?>
