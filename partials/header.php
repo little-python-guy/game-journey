247ea7e9d2c5ddcadd7395863b5ab5b5c013f568
little-python-guy <matat201@gmail.com>
2024-10-02 17:02:38 -0300

    correcting CSS href path

--- partials/header.php
+++ partials/header.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 
     <title>Teste</title>
 
