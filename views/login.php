f23bb3680f1141c535b2812366adffd27fb9d8e4
little-python-guy <matat201@gmail.com>
2024-10-02 16:59:49 -0300

    renaming inputs 'names' and 'id' properties

--- views/login.php
+++ views/login.php
@@ -2,19 +2,21 @@
 <body class="bg-login">
 
     <div class="container-sm d-flex flex-column vh-100 align-items-center justify-content-center bg-login">
+
+        <!-- COMEÇO - CARD DE LOGIN -->
         <div class="d-flex flex-column align-items-center justify-content-center rounded-4 bg-dark login-bg">
 
             <h1 class="text-white mb-3">Bem-Vindo !!!</h1>
             <p class="text-white">Entre com seus dados para prosseguir</p>
 
-            <form class="custom-width login-form" action="" method="POST">
+            <form class="custom-width login-form" action="./main-menu.php" method="POST">
                 <div class="form-floating mb-5">
-                    <input class="form-control w-100" type="text" name="usuario" id="usuario" placeholder="Digite seu nome de usuário">
-                    <label for="usuario">Usuário</label>
+                    <input class="form-control w-100" type="text" name="user" id="user" placeholder="Digite seu nome de usuário" required>
+                    <label for="user">Usuário</label>
                 </div>
                 <div class="form-floating mt-5 mb-5">
-                    <input class="form-control" type="password" name="senha" id="senha" placeholder="Digite sua senha">
-                    <label for="senha">Senha</label>
+                    <input class="form-control" type="password" name="password" id="password" placeholder="Digite sua senha" required>
+                    <label for="password">Senha</label>
                 </div>
                 <div class="d-flex justify-content-between gap-3">
                     <button type="reset" class="btn btn-primary p-3 w-50">Limpar</button>
@@ -22,8 +24,9 @@
                 </div>
             </form>
         </div>
+        <!-- FINAL - CARD DE LOGIN -->
         
-        <p class="mt-3">Não possui cadastro? Faça seu registro <a href="">aqui.</a></p>
+        <p class="text-white mt-3">Não possui cadastro? Faça seu registro <a href="">aqui.</a></p>
 
     </div>
 
