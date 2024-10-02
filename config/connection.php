208a5e21aa9e23aaeaeb8bc43460639ce0a4fb21
little-python-guy <matat201@gmail.com>
2024-10-02 17:01:13 -0300

    basic login system for testing

--- config/connection.php
+++ config/connection.php
@@ -1,10 +1,20 @@
 <?php 
 
     $host = 'localhost';
-    $username = 'admin';
-    $password = '';
-    $bd_name = 'game_journey_bd';
+    $username = 'root';
+    $db_password = '';
+    $db_name = 'game_journey_bd';
     
-    $connect = mysqli_connect($host, $username, $password, $bd_name);
+    $connect = mysqli_connect($host, $username, $db_password, $db_name);
+
+    if ($connect) {
+
+        echo "Conexão OK";
+
+    } else {
+
+        echo "Conexão Falhou";
+
+    }
 
 ?>
